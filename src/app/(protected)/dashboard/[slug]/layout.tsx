7c7d3e92d12b87483dce13536405e1e3d9072b6a
Aravind Kreshna <aravindkreshna@gmail.com>
2025-03-12 19:02:55 +0530

    spinner and navbar started

---
+++ src/app/(protected)/dashboard/[slug]/layout.tsx
@@ -0,0 +1,21 @@
+import Sidebar from '@/components/global/sidebar'
+import React from 'react'
+
+type Props = {
+    children: React.ReactNode
+    params: { slug: string}
+}
+
+const Layout = ({ children, params }: Props) => {
+    //Query
+    //WIP: Query Client fetch data
+
+
+    return(
+        <div className="p-3">
+            <Sidebar slug={params.slug} />
+        </div>
+    )
+}
+
+export default Layout
--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 type Props = {}
 
 const Page = async (props: Props) => {
-    // Server Action Inboard the user
-    //200 || 201
+    //WIP: Server Action Inboard the user
+    //WIP: 200 || 201
 
-    return <div>Page</div>
+    return <div></div>
 }
 
 export default Page
