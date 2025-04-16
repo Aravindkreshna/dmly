b71c6860a6d2bea418242139666aa0045882d12f
Aravind Kreshna <aravindkreshna@gmail.com>
2025-04-17 00:37:41 +0530

    00

---
+++ src/components/global/navbar/index.tsx
@@ -0,0 +1,18 @@
+'use client'
+import { PAGE_BREAD_CRUMBS } from '@/constants/pages'
+import { usePaths } from '@/hooks/user-nav'
+import React from 'react'
+
+type Props = {
+    slug: string
+}
+
+const Navbar = ({ slug }: Props) => {
+    const { page } = usePaths()
+    const currentPage = PAGE_BREAD_CRUMBS.includes(page) || page == slug
+
+
+    return currentPage && <div className="flex flex-col">fff fgdgg</div>
+}
+
+export default Navbar
--- src/components/global/sidebar/index.tsx
+++ src/components/global/sidebar/index.tsx
@@ -22,7 +22,7 @@ const Sidebar = ({ slug }: Props) => {
 
     return (
         <div className="w-[250px] border-[1px] radial fixed left-0 lg:inline-block border-[#545454]
-            bg-gradient-to-b from-[#768BDD] via-[#171717] to-[#768BDD] hidden bottom-0 top-0 m-3 rounded-3xl overflow-hidden">
+            bg-gradient-to-b from-[#768BDD] via-[#171717] to-[#768BDD] hidden bottom-0 top-0 m-3 rounded-3xl overflow-hidden ">
                 <div className="flex flex-col gap-y-5 w-full h-full p-3 bg-[#171717] bg-opacity-90 bg-clip-padding backdrop-filter__safari backdrop-blur-3xl ">
                     <div className="flex gap-x-2 items-center p-5 justify-center">
                         <LogoSmall />
@@ -48,7 +48,7 @@ const Sidebar = ({ slug }: Props) => {
                             <p className="text-[#989ca0]">Help</p>
                         </div>
                     </div>
-                    <SubscriptionPlan>
+                    <SubscriptionPlan type="FREE">
                         <div className="flex-1 flex flex-col justify-end">
                             <UpgradeCard/>
                         </div>
